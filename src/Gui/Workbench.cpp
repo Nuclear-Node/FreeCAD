aba4741e0b363d0bd88d5970609e1c618157c811
Phoebe Kim <phoebekim99@gmail.com>
2020-12-29 17:07:29 -0800

    removed options under help in menu bar

--- src/Gui/Workbench.cpp
+++ src/Gui/Workbench.cpp
@@ -631,10 +631,11 @@ MenuItem* StdWorkbench::setupMenuBar() const
     // Help
     MenuItem* help = new MenuItem(menuBar);
     help->setCommand("&Help");
-    *help << "Std_OnlineHelp" << "Std_FreeCADWebsite"
+   /* *help << "Std_OnlineHelp" << "Std_FreeCADWebsite"
         << "Std_FreeCADUserHub" << "Std_FreeCADPowerUserHub"
         << "Std_PythonHelp" << "Std_FreeCADForum"
         << "Std_FreeCADFAQ" << "Std_About" << "Std_WhatsThis";
+        */
 
     return menuBar;
 }
